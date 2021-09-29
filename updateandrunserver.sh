6afa7b0097a65c461aaed462d270bef80b5898d5
HugoBallee <hugo.gallee@gmail.com>
2021-09-30 01:10:47 +0200

    Update test change

--- updateandrunserver.sh
+++ updateandrunserver.sh
@@ -7,9 +7,9 @@ until $(curl --output /dev/null --silent --head --fail https://github.com); do
 done
 sudo echo "Connected" >>/home/pi/log.txt
 sudo echo "Staching" >>/home/pi/log.txt
-sudo git -C /home/pi/humiditytemperaturelogger/ stash 2>>/home/pi/log.txt
+sudo git -C /home/pi/humiditytemperaturelogger/ stash &>>/home/pi/log.txt
 sudo echo "Pulling" >>/home/pi/log.txt
-sudo git -C /home/pi/humiditytemperaturelogger/ pull 2>>/home/pi/log.txt
+sudo git -C /home/pi/humiditytemperaturelogger/ pull &>>/home/pi/log.txt
 sudo echo "Launching server." >>/home/pi/log.txt
-sudo "python3 /home/pi/humiditytemperaturelogger/humiditytemperaturelogger/manage.py runserver 0.0.0.0:8000" 2>> >>/home/pi/log.txt
+sudo "python3 /home/pi/humiditytemperaturelogger/humiditytemperaturelogger/manage.py runserver 0.0.0.0:8000" &>>/home/pi/log.txt
 sudo echo "Done" >>/home/pi/log.txt
