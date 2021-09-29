910c1de1fbd73dbba0ce24a227144e560f0b52f0
HugoBallee <hugo.gallee@gmail.com>
2021-09-29 20:53:25 +0200

    Update update and run server script

--- updateandrunserver.sh
+++ updateandrunserver.sh
@@ -8,14 +8,6 @@ echo "*        UPDATING        *"
 echo "**************************"
 git pull -f
 echo "**************************"
-echo "*   MOVING LAUNCH FILE   *"
-echo "**************************"
-cp ./updateandrunserver.sh /etc/rc.local/updateandrunserver.sh
-echo "**************************"
-echo "*     GIVING RIGHTS      *"
-echo "**************************"
-chmod +x /etc/rc.local/updateandrunserver.sh
-echo "**************************"
 echo "*    LAUNCHING SERVER    *"
 echo "**************************"
 python3 ./humiditytemperaturelogger/manage.py runserver 0.0.0.0:8000
