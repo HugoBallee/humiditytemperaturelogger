ea1763774392183a30b55e5c1bc5064511fe27e7
HugoBallee <hugo.gallee@gmail.com>
2021-09-29 20:44:32 +0200

    Update update and run server script

--- updateandrunserver.sh
+++ updateandrunserver.sh
@@ -1,6 +1,14 @@
 cd ~/humiditytemperaturelogger/
+echo "**************************"
+echo "*      SAVING STASH      *"
+echo "**************************"
 git stash
+echo "**************************"
+echo "*        UPDATING        *"
+echo "**************************"
 git pull -f
 chmod +x updateandrunserver.sh
-echo "Hello"
+echo "**************************"
+echo "*    LAUNCHING SERVER    *"
+echo "**************************"
 python3 ./humiditytemperaturelogger/manage.py runserver 0.0.0.0:8000
