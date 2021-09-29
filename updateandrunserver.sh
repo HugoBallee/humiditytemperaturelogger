daacabf40c8c2593b5da69eee11c605a2658aba4
HugoBallee <hugo.gallee@gmail.com>
2021-09-29 22:57:36 +0200

    Update test change

--- updateandrunserver.sh
+++ updateandrunserver.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 echo "1" >> /home/pi/run.txt
-cd ~/humiditytemperaturelogger/ 2>err.txt
+cd ~/humiditytemperaturelogger/ 2>/home/pi/err.txt
 echo "**************************"
 echo "*      SAVING STASH      *"
 echo "**************************"
 echo "2" >> /home/pi/run.txt
-git stash 2>>err.txt
+git stash 2>>/home/pi/err.txt
 echo "**************************"
 echo "*        UPDATING        *"
 echo "**************************"
 echo "3" >> /home/pi/run.txt
-git pull -f 2>>err.txt
+git pull -f 2>>/home/pi/err.txt
 echo "**************************"
 echo "*    LAUNCHING SERVER    *"
 echo "**************************"
 echo "4" >> /home/pi/run.txt
-python3 ./humiditytemperaturelogger/manage.py runserver 0.0.0.0:8000  2>>err.txt &
+python3 ./humiditytemperaturelogger/manage.py runserver 0.0.0.0:8000  2>>/home/pi/err.txt &
 echo "5" >> /home/pi/run.txt
