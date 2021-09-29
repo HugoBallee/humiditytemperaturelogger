c0fa3349c1746677354ca20481b236fb27634a66
HugoBallee <hugo.gallee@gmail.com>
2021-09-30 00:54:39 +0200

    Update test change

--- updateandrunserver.sh
+++ updateandrunserver.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-sudo echo "Checking connection to https://github.com ..." &>> /home/pi/log.txt
+sudo echo "Checking connection to https://github.com ..."
 until $(curl --output /dev/null --silent --head --fail https://github.com); do
-    echo "." &>> /home/pi/log.txt
+    echo "."
     sleep 5
 done
-sudo echo "Connected" &>> /home/pi/log.txt
-sudo echo "Staching" &>> /home/pi/log.txt
-sudo git -C /home/pi/humiditytemperaturelogger/ stash &>>/home/pi/log.txt
-sudo echo "Pulling" &>> /home/pi/log.txt
-sudo git -C /home/pi/humiditytemperaturelogger/ pull &>>/home/pi/log.txt
-sudo echo "Launching server." &>> /home/pi/log.txt
-sudo python3 /home/pi/humiditytemperaturelogger/humiditytemperaturelogger/manage.py runserver 0.0.0.0:8000  &>>/home/pi/log.txt &
-sudo echo "Done" &>> /home/pi/log.txt
+sudo echo "Connected"
+sudo echo "Staching"
+sudo git -C /home/pi/humiditytemperaturelogger/ stash
+sudo echo "Pulling"
+sudo git -C /home/pi/humiditytemperaturelogger/ pull
+sudo echo "Launching server."
+sudo "python3 /home/pi/humiditytemperaturelogger/humiditytemperaturelogger/manage.py runserver 0.0.0.0:8000"
+sudo echo "Done"
