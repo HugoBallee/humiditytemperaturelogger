61cedb3546c805562f8288556f787100e3b28116
HugoBallee <hugo.gallee@gmail.com>
2021-09-29 23:06:01 +0200

    Update test change

--- updateandrunserver.sh
+++ updateandrunserver.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-echo "1" >> /home/pi/run.txt
-cd home/pi/humiditytemperaturelogger/ 2>/home/pi/err.txt
-echo "**************************"
-echo "*      SAVING STASH      *"
-echo "**************************"
-echo "2" >> /home/pi/run.txt
-git stash 2>>/home/pi/err.txt
-echo "**************************"
-echo "*        UPDATING        *"
-echo "**************************"
-echo "3" >> /home/pi/run.txt
-git pull -f 2>>/home/pi/err.txt
-echo "**************************"
-echo "*    LAUNCHING SERVER    *"
-echo "**************************"
-echo "4" >> /home/pi/run.txt
+sudo echo "1" >> /home/pi/run.txt
+sudo cd home/pi/humiditytemperaturelogger/ 2>/home/pi/err.txt
+sudo echo "**************************"
+sudo echo "*      SAVING STASH      *"
+sudo echo "**************************"
+sudo echo "2" >> /home/pi/run.txt
+sudo git stash 2>>/home/pi/err.txt
+sudo echo "**************************"
+sudo echo "*        UPDATING        *"
+sudo echo "**************************"
+sudo echo "3" >> /home/pi/run.txt
+sudo git pull -f 2>>/home/pi/err.txt
+sudo echo "**************************"
+sudo echo "*    LAUNCHING SERVER    *"
+sudo echo "**************************"
+sudo echo "4" >> /home/pi/run.txt
 sudo python3 ./humiditytemperaturelogger/manage.py runserver 0.0.0.0:8000  2>>/home/pi/err.txt &
-echo "5" >> /home/pi/run.txt
+sudo echo "5" >> /home/pi/run.txt
