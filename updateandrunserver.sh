3ed28dbb91dcbd5ab2dc38d12f9aa1f6d57b52a0
HugoBallee <hugo.gallee@gmail.com>
2021-09-29 20:48:13 +0200

    Update update and run server script

--- updateandrunserver.sh
+++ updateandrunserver.sh
@@ -7,7 +7,13 @@ echo "**************************"
 echo "*        UPDATING        *"
 echo "**************************"
 git pull -f
+echo "**************************"
+echo "*   MOVING LAUNCH FILE   *"
+echo "**************************"
 cp ./updateandrunserver.sh /etc/rc.local/updateandrunserver.sh
+echo "**************************"
+echo "*     GIVING RIGHTS      *"
+echo "**************************"
 chmod +x /etc/rc.local/updateandrunserver.sh
 echo "**************************"
 echo "*    LAUNCHING SERVER    *"
