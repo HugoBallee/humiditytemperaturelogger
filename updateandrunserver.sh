6cd9f95b07c12fbcbc87dceab41424a8688bc229
HugoBallee <hugo.gallee@gmail.com>
2021-09-29 23:15:53 +0200

    Update test change

--- updateandrunserver.sh
+++ updateandrunserver.sh
@@ -4,7 +4,7 @@ sudo echo "**************************"
 sudo echo "*      SAVING STASH      *"
 sudo echo "**************************"
 sudo echo "1" >> /home/pi/run.txt
-sudo git -C /home/pi/humiditytemperaturelogger/ stash 2>>/home/pi/err.txt
+sudo git -C /home/pi/humiditytemperaturelogger/ stash 2>/home/pi/err.txt
 sudo echo "**************************"
 sudo echo "*        UPDATING        *"
 sudo echo "**************************"
@@ -14,5 +14,5 @@ sudo echo "**************************"
 sudo echo "*    LAUNCHING SERVER    *"
 sudo echo "**************************"
 sudo echo "3" >> /home/pi/run.txt
-sudo python3 ./humiditytemperaturelogger/manage.py runserver 0.0.0.0:8000  2>>/home/pi/err.txt &
+sudo python3 /home/pi/humiditytemperaturelogger/manage.py runserver 0.0.0.0:8000  2>>/home/pi/err.txt &
 sudo echo "4" >> /home/pi/run.txt
