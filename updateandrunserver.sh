e3ae8a9f90fd830dda457b7e4a4c3fca2519066b
HugoBallee <hugo.gallee@gmail.com>
2021-09-30 17:23:32 +0200

    Update test change

--- updateandrunserver.sh
+++ updateandrunserver.sh
@@ -1,10 +1,10 @@
 WD="/home/pi/humiditytemperaturelogger/"
 
-echo "start" >"${WD}log.txt"
-source "${WD}webservervenv/bin/activate" &>>"${WD}log.txt"
+echo "start"
+source "${WD}webservervenv/bin/activate"
 until $(curl --output /dev/null --silent --head --fail https://github.com); do
     sleep 5
 done
-git -C "${WD}" stash &>>"${WD}log.txt"
-git -C "${WD}" pull &>>"${WD}log.txt"
-python3 "${WD}humiditytemperaturelogger/manage.py" runserver 0.0.0.0:8000 &>>"${WD}log.txt"
+git -C "${WD}" stash
+git -C "${WD}" pull
+python3 "${WD}humiditytemperaturelogger/manage.py" runserver 0.0.0.0:8000 &
