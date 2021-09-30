09feb1f5220c17a52da482485b0a2f0c11027845
HugoBallee <hugo.gallee@gmail.com>
2021-09-30 16:59:04 +0200

    Update test change

--- updateandrunserver.sh
+++ updateandrunserver.sh
@@ -1,7 +1,7 @@
-source /home/pi/humiditytemperaturelogger/webservervenv/bin/activate &>>/home/pi/log.txt
+source /home/pi/humiditytemperaturelogger/webservervenv/bin/activate &>/home/pi/humiditytemperaturelogger/log.txt
 until $(curl --output /dev/null --silent --head --fail https://github.com); do
     sleep 5
 done
-git -C /home/pi/humiditytemperaturelogger/ stash &>>/home/pi/log.txt
-git -C /home/pi/humiditytemperaturelogger/ pull &>>/home/pi/log.txt
-python3 /home/pi/humiditytemperaturelogger/humiditytemperaturelogger/manage.py runserver 0.0.0.0:8000 &>>/home/pi/log.txt
+git -C /home/pi/humiditytemperaturelogger/ stash &>>/home/pi/humiditytemperaturelogger/log.txt
+git -C /home/pi/humiditytemperaturelogger/ pull &>>/home/pi/humiditytemperaturelogger/log.txt
+python3 /home/pi/humiditytemperaturelogger/humiditytemperaturelogger/manage.py runserver 0.0.0.0:8000 &>>/home/pi/humiditytemperaturelogger/log.txt
