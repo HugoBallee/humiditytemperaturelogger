2ea7440dcc6f55a763537f86c7977a36b2721571
HugoBallee <hugo.gallee@gmail.com>
2021-10-01 22:52:47 +0200

    Adding logger

--- humiditytemperaturelogger/humiditytemperaturelogger/settings.py
+++ humiditytemperaturelogger/humiditytemperaturelogger/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'k@m#+k8$l!escl00cywsm$d*ch=n^9segr#^fo5rme+qf@wg0t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
