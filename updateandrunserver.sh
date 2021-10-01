4eabd5be3727d331749272b195c30d376c8caafa
HugoBallee <hugo.gallee@gmail.com>
2021-10-01 12:41:12 +0200

    Rename app logger to front

--- updateandrunserver.sh
+++ updateandrunserver.sh
@@ -5,6 +5,8 @@ echo "start"
 until $(curl --output /dev/null --silent --head --fail https://github.com); do
     sleep 5
 done
+git -C "${1}" config user.name "hugoballee"
+git -C "${1}" config user.email "hugo.gallee@gmail.com"
 git -C "${1}" stash
 git -C "${1}" pull
 python3 "${1}humiditytemperaturelogger/manage.py" runserver 0.0.0.0:8000 &
