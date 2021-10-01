e632431af0f8811f67285621423d16c12db30ab5
HugoBallee <hugo.gallee@gmail.com>
2021-10-01 14:00:05 +0200

    Adding logger

--- humiditytemperaturelogger/logger/logger.py
+++ humiditytemperaturelogger/logger/logger.py
@@ -3,6 +3,6 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from logger import loggerApi
 
 def start():
-    scheduler = BackgroundScheduler()
+    scheduler = BackgroundScheduler({'apscheduler.job_defaults.max_instances': 2})
     scheduler.add_job(loggerApi.log, 'interval', minutes=1)
     scheduler.start()
